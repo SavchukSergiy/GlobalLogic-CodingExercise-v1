3fda0173e33dbebf7e2fec73702cfba6aa623656
Savchuk Sergiy <savchuksergiy94@gmail.com>
2021-03-19 11:28:21 +0200

    Added score broad

---
+++ FootballScoreBoard/FootballScoreBoard/ScoreBoard.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballScoreBoard
+{
+    public class ScoreBoard
+    {
+        // List of all matches.
+        public List<Match> Matches = new List<Match>();
+
+        // Add new match to the sroce broad.
+        public Match Add() 
+        {
+            return null;
+        }
+
+        // Finish match.
+        public void Finish()
+        {
+            
+        }
+
+        // Update match score.
+        public void UpdateScore(Match match)
+        {
+            
+        }
+
+        // Get a summary of games by total score.
+        public List<Match> GetSummary()
+        {
+            return null;
+        }
+    }
+}
