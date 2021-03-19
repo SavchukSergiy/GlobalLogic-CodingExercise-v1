1defc625412b37b1c802a05241ac47fb1d482034
Savchuk Sergiy <savchuksergiy94@gmail.com>
2021-03-19 11:20:07 +0200

    added match essence

---
+++ FootballScoreBoard/FootballScoreBoard/Match.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballScoreBoard
+{
+    public class Match
+    {
+        Team HomeTeam { get; set; }
+        Team AwayTeam { get; set; }
+
+        short HomeTeamScore { get; set; }
+        short AwayTeamScore{get;set;}
+
+        public Match(Team homeTeam, Team awayTeam,  short homeTeamScore = 0, short awayTeamScore = 0)
+        {
+            HomeTeam = homeTeam;
+            AwayTeam = awayTeam;
+        }
+    }
+}
