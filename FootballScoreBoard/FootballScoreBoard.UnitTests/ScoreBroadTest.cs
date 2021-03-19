4290104c9d05a6c66abd5481af8e082b4076b3fe
Savchuk Sergiy <savchuksergiy94@gmail.com>
2021-03-19 12:30:49 +0200

    fix add test

--- FootballScoreBoard/FootballScoreBoard.UnitTests/ScoreBroadTest.cs
+++ FootballScoreBoard/FootballScoreBoard.UnitTests/ScoreBroadTest.cs
@@ -6,16 +6,21 @@ namespace FootballScoreBoard.UnitTests
     public class ScoreBroadTest
     {
         [TestMethod]
-        public void AddMatch_Match_AddToList()
+        public void AddMatch_Match_MatchNotNull()
         {
             // Arrange
-            
+            var board = new ScoreBoard();
+            var homeTeam = new HomeTeam { Name = "team1", Country = "Ukraine", Ñoach = "Cool man", TeamScore = 0 };
+            var awayTeam = new AwayTeam { Name = "team2", Country = "USA", Ñoach = "Cool man2", TeamScore = 0 };
+
+            var match = new Match(homeTeam, awayTeam);
 
             // Act
-            
+
+            board.AddMatch(match);
 
             // Assert
-            
+            Assert.IsNotNull(match);
         }
     }
 }
