7dfc6860f7310c66882734cb8040de31c6b3c4cd
Savchuk Sergiy <savchuksergiy94@gmail.com>
2021-03-19 12:19:29 +0200

    Added test for AddMatch

---
+++ FootballScoreBoard/FootballScoreBoard.UnitTests/ScoreBroadTest.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FootballScoreBoard.UnitTests
+{
+    [TestClass]
+    public class ScoreBroadTest
+    {
+        [TestMethod]
+        public void AddMatch_Match_AddToList()
+        {
+            // Arrange
+            
+
+            // Act
+            
+
+            // Assert
+            
+        }
+    }
+}
