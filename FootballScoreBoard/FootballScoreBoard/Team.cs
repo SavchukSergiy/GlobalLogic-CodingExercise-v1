179c8b44fdcb46d3c9b624c635af0f1937855037
Savchuk Sergiy <savchuksergiy94@gmail.com>
2021-03-19 11:06:58 +0200

    added team essence

---
+++ FootballScoreBoard/FootballScoreBoard/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballScoreBoard
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Сoach { get; set; }
+    }
+}
